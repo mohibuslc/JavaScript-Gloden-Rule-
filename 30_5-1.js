306b64295f6f18e0b88ebc640502d4b93999e3e9
mohibuslc <mohib_uslc@outlook.com>
2022-05-08 00:03:29 +0100

    30_5-1 Five Javascript Findemental

---
+++ 30_5-1.js
@@ -0,0 +1,74 @@
+
+// Variable : let  and const 
+
+
+let money = 20 ;
+
+money = 12 ;
+
+
+//console.log(money)\
+
+
+const personDetails = {
+
+
+    name : 'Kudus',
+
+    age : 24 ,
+
+    address : 'Tillaghor'
+};
+
+const statement = ` This is name ${personDetails.name} Age: ${personDetails.age} Address is : ${personDetails.address}`
+
+//console.log(statement) 
+
+// const balance = 1100 ;
+
+// const phone = 900 ; 
+
+// if( phone < balance){
+
+
+//     console.log(" you have enough balance .. can u do make purches ");
+
+// } else {
+
+//     console.log(" can you do buy ");
+
+// }
+
+
+// if else statement 
+
+
+// let  phone = 67 ;
+
+// if( phone > 88){
+
+//     console.log(" A");
+// }else if( phone >= 70){
+
+//     console.log("B");
+// }
+// else if( phone >=67){
+
+//     console.log( " condition is not good ")
+// }
+
+//  if else Statement 
+
+const age = 19;
+
+let citizen ; 
+
+if( age >= 18){
+
+    citizen = " can do vote ";
+}else {   
+
+    citizen = "cannot do vote "
+}
+
+console.log(citizen)
